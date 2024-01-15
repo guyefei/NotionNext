620d40f2f98512b66ff545ceeeea2b0d1a8e9408
guyefei <89022058+guyefei@users.noreply.github.com>
2024-01-15 21:49:08 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,17 +5,17 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://vanfitop.com' },
+    { title: '访问跨境出海知识获取更多帮助', url: 'https://vanfitop.com' }
   ],
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '分享跨境出海',
+  HEO_HERO_TITLE_2: '与外贸营销',
+  HEO_HERO_TITLE_3: 'Vavfitop.COM',
+  HEO_HERO_TITLE_4: 'B2B出海',
+  HEO_HERO_TITLE_5: 'Siteground 轻松自建网站',
+  HEO_HERO_TITLE_LINK: 'https://vanfitop.com',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
